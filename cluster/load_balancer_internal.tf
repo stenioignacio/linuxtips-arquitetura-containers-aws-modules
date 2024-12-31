e1a8fe3b3c7596b6c505da6f1c3eadaf516f59fc
stenioignacio <stenioignacio@gmail.com>
2024-12-31 10:08:35 -0300

    Fix target Group Attach - Internal LB

--- cluster/load_balancer_internal.tf
+++ cluster/load_balancer_internal.tf
@@ -45,7 +45,7 @@ resource "aws_lb" "internal" {
 
 resource "aws_lb_listener" "internal" {
   load_balancer_arn = aws_lb.internal.arn
-  port              = 80
+  port              = "80"
   protocol          = "HTTP"
   default_action {
     type = "fixed-response"
